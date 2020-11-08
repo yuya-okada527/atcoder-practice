1d589b53a4df6a35bd79c4a5b8a8c1c0d1bf4c26
yuya-okada527 <yuya.okada527@gmail.com>
2020-11-08 19:15:52 +0900

    add d problem

---
+++ python/contest/abc/181/d.py
@@ -0,0 +1,15 @@
+# s = input()
+
+# eight_times = [str(i) for i in range(1, 1001) if i % 8 == 0]
+
+# for eight_time in eight_times:
+#     flg = True
+#     for e in eight_time:
+#         if e not in s:
+#             flg = False
+    
+#     if flg:
+#         print("Yes")
+#         exit()
+
+# print("No")
