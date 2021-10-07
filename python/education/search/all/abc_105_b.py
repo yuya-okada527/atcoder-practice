0ba642d82a9b418b284b6b237db97775e444e883
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-07 23:50:21 +0900

    add abc 221

--- python/education/search/all/abc_105_b.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-for i in range(100//4+1):
-    for j in range(100//7+1):
-        if i * 4 + j * 7 == N:
-            print("Yes")
-            exit()
-print("No")
--- python/education/search/all/abc_95_c.py
+++
@@ -1,8 +0,0 @@
-A, B, C, X, Y = map(int, input().split())
-ans = 5000*(10**5) * 2 + 1
-for ab in range(max(X, Y) * 2 + 1):
-    price = ab * C
-    price += A * (X - ab // 2) if (X - ab // 2) > 0 else 0
-    price += B * (Y - ab // 2) if (Y - ab // 2) > 0 else 0
-    ans = min(ans, price)
-print(ans)
