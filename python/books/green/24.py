6a6aa3ab8e0df841a000cdc6f02f633b1a3c5a5e
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-10 13:37:05 +0900

    add 24

---
+++ python/books/green/24.py
@@ -0,0 +1,23 @@
+N, M, X = map(int, input().split())
+C = []
+A = []
+for _ in range(N):
+    CA = list(map(int, input().split()))
+    C.append(CA[0])
+    A.append(CA[1:])
+
+ans = 10 ** 10
+for pattern in range(1<<N):
+    cost = 0
+    skill = [0] * M
+    for book in range(N):
+        if pattern >> book & 1 == 1:
+            cost += C[book]
+            for j in range(M):
+                skill[j] += A[book][j]
+    if X <= min(skill):
+        ans = min(ans, cost)
+if ans == 10 ** 10:
+    print(-1)
+else:
+    print(ans)
