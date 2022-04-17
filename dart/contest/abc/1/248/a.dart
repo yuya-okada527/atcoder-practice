de1a8bfa6604b6d5ac413245ec2c7a4bb50f6b9f
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-18 00:18:07 +0900

    divide implements

--- dart/contest/abc/1/248/a.dart
+++ dart/contest/abc/1/248/a.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
 void main() {
+  f1();
+}
+
+
+void f1() {
   String val = stdin.readLineSync() ?? "";
   for (var i in List<int>.generate(10, (index) => index)) {
     if (!val.contains(i.toString())) {
