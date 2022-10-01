b1ce2e5a2c70fc365f153485f8ce36537a7be37f
yuya-okada527 <yuya.okada527@gmail.com>
2022-10-01 13:37:54 +0900

    refactor 13.py

--- python/books/atcoder/13.py
+++ python/books/atcoder/13.py
@@ -1,8 +1,6 @@
-from collections import defaultdict
+from collections import Counter
 
 n = int(input())
 S = ["".join(sorted(input())) for _ in range(n)]
-count = defaultdict(int)
-for s in S:
-    count[s] += 1
+count = Counter(S)
 print(sum([n*(n-1)//2 for n in count.values()]))
