2c53ebb8014a5f4eb085e1495f9517e3746c7bc0
yuya-okada527 <yuya.okada527@gmail.com>
2022-10-16 16:44:15 +0900

    add 11f

--- python/books/atcoder/11f.py
+++ python/books/atcoder/11f.py
@@ -1,3 +1,20 @@
 """
 ABC157 C
 """
+n, m = map(int, input().split())
+SC = [list(map(int, input().split())) for _ in range(m)]
+if m == 0:
+    if n == 1:
+        print(0)
+        exit()
+    print("1" + "0" * (n-1))
+    exit()
+for i in range(10**n):
+    flg = True
+    for (s, c) in SC:
+        if len(str(i)) != n or str(i)[s-1] != str(c):
+            flg = False
+    if flg:
+        print(i)
+        exit()
+print(-1)
