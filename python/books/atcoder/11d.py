135db8e3c9530440689698eb7ccd06d5fb974b26
yuya-okada527 <yuya.okada527@gmail.com>
2022-10-16 15:42:29 +0900

    add 11d.py

--- python/books/atcoder/11d.py
+++ python/books/atcoder/11d.py
@@ -1,3 +1,10 @@
 """
 典型90 55
 """
+from itertools import combinations
+n, p, q = map(int, input().split())
+A = list(map(int, input().split()))
+ans = 0
+for c in combinations(A, 5):
+    ans += c[0] * c[1]%p * c[2]%p * c[3]%p * c[4] % p == q
+print(ans)
