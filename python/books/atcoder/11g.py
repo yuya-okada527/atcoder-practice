764ae9f2ae55e3c473f2e083e11e3d1c129ef6e4
yuya-okada527 <yuya.okada527@gmail.com>
2022-10-16 17:02:34 +0900

    add 11g.py

--- python/books/atcoder/11g.py
+++ python/books/atcoder/11g.py
@@ -1,3 +1,14 @@
 """
 ABC195 B
 """
+a, b, w = map(int, input().split())
+w *= 1000
+min_count = w
+max_count = -1
+for i in range(1, w+1):
+    left = a * i
+    right = b * i
+    if left <= w <= right:
+        min_count = min(i, min_count)
+        max_count = max(i, max_count)
+print("UNSATISFIABLE" if max_count == -1 else f"{min_count} {max_count}")
