e9c67c8522ebd7ca4f8fcdcc931bf0d6d778e32c
yuya-okada527 <yuya.okada527@gmail.com>
2022-10-16 15:53:41 +0900

    add 11e.py

--- python/books/atcoder/11e.py
+++ python/books/atcoder/11e.py
@@ -1,3 +1,12 @@
 """
 典型90 16
 """
+n = int(input())
+a, b, c = map(int, input().split())
+ans = 10000
+for i in range(10000):
+    for j in range(10000-i):
+        k = (n - (a*i + b*j)) // c
+        if k >= 0 and (n- (a*i + b*j)) % c == 0:
+            ans = min(ans, i+j+k)
+print(ans)
