82842c229191b0abf0b2501f4a7026195275c326
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-25 23:52:57 +0900

    add 282 a

---
+++ rust/contest/src/abc/282/a.rs
@@ -0,0 +1,45 @@
+use proconio::input;
+
+
+
+fn main() {
+    let alphabet = vec![
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ];
+    input! {
+        mut k: i32,
+    }
+    for s in alphabet {
+        print!("{}", s);
+        k -= 1;
+        if k == 0 {
+            break;
+        }
+    }
+    println!();
+}
