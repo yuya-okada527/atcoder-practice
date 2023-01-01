59ecb9a67a15edbbb6643cc618aa5dd1a64702ac
yuya-okada527 <yuya.okada527@gmail.com>
2023-01-01 13:02:53 +0900

    partial commit

---
+++ rust/contest/src/abc/271/a.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn f(n: i32, &mut ans: String) -> String {
+    if n % 16 == 0 {
+        // ans = ans + (n / )
+    } else {
+        ans
+    }
+}
+
+fn main() {
+    input! {
+        n: i32,
+    }
+
+}
