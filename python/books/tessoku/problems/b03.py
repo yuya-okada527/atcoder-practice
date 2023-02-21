5272d662b075e681609737df64c17201daca7289
yuya-okada527 <yuya.okada527@gmail.com>
2023-02-21 22:18:30 +0900

    fix bugs

--- python/books/tessoku/problems/b03.py
+++ python/books/tessoku/problems/b03.py
@@ -1,9 +1,10 @@
 n = int(input())
 A = list(map(int, input().split()))
 
-for i in A:
-    for j in A:
-        if (1000 - (i + j)) in A:
-            print("Yes")
-            exit()
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if A[i] + A[j] + A[k] == 1000:
+                print("Yes")
+                exit()
 print("No")
