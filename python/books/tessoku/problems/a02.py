abb01c6e422c62ddd0e3586bfbd1d2ec01ac2c15
yuya-okada527 <yuya.okada527@gmail.com>
2023-02-21 21:58:06 +0900

    add b02.py

---
+++ python/books/tessoku/problems/a02.py
@@ -0,0 +1,6 @@
+n, x = map(int, input().split())
+A = list(map(int, input().split()))
+if x in A:
+    print("Yes")
+else:
+    print("No")
---
+++ python/books/tessoku/problems/b02.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+for i in range(a, b+1):
+    if 100 % i == 0:
+        print("Yes")
+        exit()
+print("No")
