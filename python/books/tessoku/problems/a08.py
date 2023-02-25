ba21ff892b88a724eb3b070bb629d55ff51d168e
yuya-okada527 <yuya.okada527@gmail.com>
2023-02-25 21:39:37 +0900

    add a08.py

--- python/books/tessoku/problems/a08.py
+++ python/books/tessoku/problems/a08.py
@@ -3,8 +3,14 @@ X = [list(map(int, input().split())) for _ in range(h)]
 q = int(input())
 Q = [list(map(int, input().split())) for _ in range(q)]
 
+CUMS = [[0 for _ in range(w)] for _ in range(h)]
+for i in range(h):
+    for j in range(w):
+        if i == 0: CUMS[i][j] = X[i][j]
+        else: CUMS[i][j] = CUMS[i-1][j] + X[i][j]
 for a, b, c, d in Q:
-    ans = 0
-    for x in X[a-1:c] if c != h else X[a-1:]:
-        ans += sum(x[b-1:d] if d != w else x[b-1:])
-    print(ans)
+    a_row = CUMS[a-2] if a != 1 else [0 for _ in range(w)]
+    c_row = CUMS[c-1]
+    a_row_column = a_row[b-1:d] if d != w else a_row[b-1:]
+    c_row_column = c_row[b-1:d] if d != w else c_row[b-1:]
+    print(sum(c_row_column) - sum(a_row_column))
