9762aa4ca0ac0ad8a454a0a203ee63c62ebbd3dc
yuya-okada527 <yuya.okada527@gmail.com>
2023-02-27 22:55:04 +0900

    archive a06.py and a07.py

--- python/books/tessoku/problems/archive/a06.py
+++ python/books/tessoku/problems/archive/a06.py
@@ -1,11 +1,11 @@
 n, q = map(int, input().split())
 A = list(map(int, input().split()))
-cums = [0]
-for i in range(len(A)):
-    cums.append(cums[i] + A[i])
 LR = []
 for _ in range(q):
     l, r = map(int, input().split())
     LR.append((l, r))
+cums = [0]
+for i in range(n):
+    cums.append(cums[i] + A[i])
 for l, r in LR:
     print(cums[r] - cums[l-1])
--- python/books/tessoku/problems/archive/a07.py
+++ python/books/tessoku/problems/archive/a07.py
@@ -5,11 +5,11 @@ for _ in range(n):
     l, r = map(int, input().split())
     diff[l-1] += 1
     if r != d: diff[r] -= 1
-cums = [0 for _ in range(d)]
+cums = []
 for i in range(d):
-    if i == 0:
-        cums[i] = diff[i]
-    else:
-        cums[i] = cums[i-1] + diff[i]
+    if i == 0: cums.append(diff[0])
+    else: cums.append(cums[i-1] + diff[i])
+# print(diff)
+# print(cums)
 for c in cums:
     print(c)
