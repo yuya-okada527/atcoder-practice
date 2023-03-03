7f8737f3f8d5d200094c1499e85bed2d546df917
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-03 21:22:45 +0900

    add b06.py

---
+++ python/books/tessoku/problems/b06.py
@@ -0,0 +1,20 @@
+n = int(input())
+A = [0] + list(map(int, input().split()))
+Z = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    Z[i] = Z[i-1] + A[i]
+q = int(input())
+LR = []
+for _ in range(q):
+    l, r = map(int, input().split())
+    LR.append((l, r))
+for l, r in LR:
+    num = r - l + 1
+    n_win = Z[r] - Z[l-1]
+    n_lose = num - n_win
+    if n_win == n_lose:
+        print("draw")
+    elif n_win > n_lose:
+        print("win")
+    else:
+        print("lose")
