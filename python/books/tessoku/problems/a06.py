a251df93ee14e838420759612f8aab87ea1f79eb
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-03 21:08:37 +0900

    fix index

--- python/books/tessoku/problems/a06.py
+++ python/books/tessoku/problems/a06.py
@@ -1,8 +1,8 @@
 n, q = map(int, input().split())
-A = list(map(int, input().split()))
+A = [0] + list(map(int, input().split()))
 Z = [0 for _ in range(n+1)]
 for i in range(1, n+1):
-    Z[i] = Z[i-1] + A[i-1]
+    Z[i] = Z[i-1] + A[i]
 LR = []
 for _ in range(q):
     l, r = map(int, input().split())
