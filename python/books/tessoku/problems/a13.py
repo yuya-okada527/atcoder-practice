36afef46e4e97f3aee42f0e37e49dea8c6a523ad
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-05 12:41:57 +0900

    a13 as binary search

--- python/books/tessoku/problems/a13.py
+++ python/books/tessoku/problems/a13.py
@@ -1,11 +1,8 @@
+from bisect import bisect
+
 n, k = map(int, input().split())
 A = list(map(int, input().split()))
 ans = 0
 for i in range(n):
-    for j in range(i+1, n):
-        d = A[j] - A[i]
-        if d <= k:
-            ans += 1
-        else:
-            break
+    ans += bisect(A, A[i]+k) - i - 1
 print(ans)
