5b387244cafd655718567e8afb90806df2844f8c
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-05 13:11:21 +0900

    a13 as sucking method

--- python/books/tessoku/problems/a13.py
+++ python/books/tessoku/problems/a13.py
@@ -1,8 +1,9 @@
-from bisect import bisect
-
 n, k = map(int, input().split())
-A = list(map(int, input().split()))
+A = [0] + list(map(int, input().split()))
 ans = 0
-for i in range(n):
-    ans += bisect(A, A[i]+k) - i - 1
+r = 1
+for i in range(1, n+1):
+    while r < n and A[r+1] - A[i] <= k:
+        r += 1
+    ans += r - i
 print(ans)
