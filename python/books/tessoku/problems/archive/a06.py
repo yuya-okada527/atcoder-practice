4659e293db80ee3cbefc6662f09ddfa3edf5ada0
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-05 18:39:25 +0900

    archive a06.py

--- python/books/tessoku/problems/archive/a06.py
+++ python/books/tessoku/problems/archive/a06.py
@@ -1,11 +1,11 @@
 n, q = map(int, input().split())
-A = list(map(int, input().split()))
-Z = [0 for _ in range(n+2)]
+A = [0] + list(map(int, input().split()))
+Z = [0 for _ in range(n+1)]
 for i in range(1, n+1):
-    Z[i] = Z[i-1] + A[i-1]
-ans = []
+    Z[i] = Z[i-1] + A[i]
+LR = []
 for _ in range(q):
     l, r = map(int, input().split())
-    ans.append(Z[r] - Z[l-1])
-for a in ans:
-    print(a)
+    LR.append((l, r))
+for l, r in LR:
+    print(Z[r] - Z[l-1])
