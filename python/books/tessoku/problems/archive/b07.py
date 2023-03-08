77a66801fa09ad9e4e01704d51f5fab2e800d452
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-08 22:19:53 +0900

    commit all

--- python/books/tessoku/problems/archive/b07.py
+++ python/books/tessoku/problems/archive/b07.py
@@ -1,13 +1,12 @@
 t = int(input())
 n = int(input())
-diff = [0 for _ in range(t)]
+DIFF = [0 for _ in range(t+2)]
 for _ in range(n):
     l, r = map(int, input().split())
-    diff[l] += 1
-    if r != t: diff[r] -= 1
-cums = []
-for i in range(t):
-    if i == 0: cums.append(diff[i])
-    else: cums.append(cums[i-1] + diff[i])
-for c in cums:
-    print(c)
+    DIFF[l] += 1
+    DIFF[r] -= 1
+Z = [0 for _ in range(t+2)]
+for i in range(t+2):
+    Z[i] = Z[i-1] + DIFF[i]
+for i in Z[:-2]:
+    print(i)
