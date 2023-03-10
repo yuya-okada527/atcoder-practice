9f32d92369289655a7eaea1be24f59a2e7d4c4bf
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-10 09:38:21 +0900

    commit all

--- python/books/tessoku/problems/archive/b08.py
+++ python/books/tessoku/problems/archive/b08.py
@@ -1,18 +1,23 @@
-N_DIM = 1509
+from pprint import pprint as print
+SIZE = 1502
+
 n = int(input())
-XY = [[0 for _ in range(N_DIM)] for _ in range(N_DIM)]
+X = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
 for _ in range(n):
     x, y = map(int, input().split())
-    XY[x][y] += 1
-Z = [[0 for _ in range(N_DIM)] for _ in range(N_DIM)]
-for x in range(1, N_DIM):
-    for y in range(1, N_DIM):
-        Z[x][y] = Z[x-1][y] + XY[x][y]
-for x in range(1, N_DIM):
-    for y in range(1, N_DIM):
-        Z[x][y] = Z[x][y-1] + Z[x][y]
+    X[x][y] += 1
+Z = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
+for i in range(1, SIZE-1):
+    for j in range(1, SIZE-1):
+        Z[i][j] = Z[i-1][j] + X[i][j]
+for i in range(1, SIZE-1):
+    for j in range(1, SIZE-1):
+        Z[i][j] = Z[i][j-1] + Z[i][j]
 q = int(input())
-Q = [list(map(int, input().split())) for _ in range(q)]
+Q = []
+for _ in range(q):
+    a, b, c, d = map(int, input().split())
+    Q.append((a, b, c, d))
 for a, b, c, d in Q:
-    ans = Z[c][d] - Z[c][b-1] - Z[a-1][d] + Z[a-1][b-1]
+    ans = Z[c][d] - Z[a-1][d] - Z[c][b-1] + Z[a-1][b-1]
     print(ans)
