bc876644452dc34a55e2347acac19a2f09531b98
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-10 22:06:22 +0900

    commit all

--- python/books/tessoku/problems/archive/a11.py
+++ python/books/tessoku/problems/archive/a11.py
@@ -1,11 +1,10 @@
 n, x = map(int, input().split())
 A = list(map(int, input().split()))
-
 left = 0
 right = n
 while left < right:
-    mid = (right - left) // 2 + left
+    mid = (left+right) // 2
     if A[mid] == x: break
     elif A[mid] > x: right = mid
-    else: left = mid
+    else: left = mid + 1
 print(mid+1)
