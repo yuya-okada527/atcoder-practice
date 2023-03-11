cec9d2e66d1b7d99d6c77ef5367fd1abcb212ae2
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-11 12:20:00 +0900

    commit all

--- python/books/tessoku/problems/archive/b11.py
+++ python/books/tessoku/problems/archive/b11.py
@@ -1,10 +1,8 @@
-import bisect
+from bisect import bisect_left
 
 n = int(input())
 A = sorted(list(map(int, input().split())))
-def f(lst, x):
-    return bisect.bisect_left(lst, x)
 q = int(input())
-for _ in range(q):
-    x = int(input())
-    print(f(A, x))
+X = [int(input()) for _ in range(q)]
+for x in X:
+    print(bisect_left(A, x))
--- python/books/tessoku/problems/b11.py
+++
@@ -1,8 +0,0 @@
-from bisect import bisect_left
-
-n = int(input())
-A = sorted(list(map(int, input().split())))
-q = int(input())
-X = [int(input()) for _ in range(q)]
-for x in X:
-    print(bisect_left(A, x))
