ff7212da08ef11ad5f0dc49354702adc2b5e2b38
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-13 23:26:20 +0900

    commit all

---
+++ python/books/tessoku/problems/a12.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+A = list(map(int, input().split()))
+left = 0
+right = 10**9
+while left < right:
+    mid = (left+right)//2
+    num = sum(map(lambda x: mid//x, A))
+    if num >= k: right = mid
+    else: left = mid + 1
+print(left)
