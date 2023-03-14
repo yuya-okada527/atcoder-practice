3d8c563aaa1b11fd6786063ab885135fdc6ade7c
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-14 22:11:23 +0900

    commit all

---
+++ python/books/tessoku/problems/a17.py
@@ -0,0 +1,17 @@
+n = int(input())
+A = [0]*2 + list(map(int, input().split()))
+B = [0]*3 + list(map(int, input().split()))
+DP = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    if i == 1: DP[i] = 0
+    elif i == 2: DP[i] = A[i]
+    else: DP[i] = min(DP[i-1]+A[i], DP[i-2]+B[i])
+pos = n
+path = [n]
+while True:
+    if DP[pos] - DP[pos-1] == A[pos]: pos -= 1
+    else: pos -= 2
+    path.append(pos)
+    if pos == 1: break
+print(len(path))
+print(*path[::-1], sep=" ")
