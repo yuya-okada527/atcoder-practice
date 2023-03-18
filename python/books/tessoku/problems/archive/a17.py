a34a66c2b0eec2a28eacc916554961800b6e1043
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 16:52:07 +0900

    commit all

--- python/books/tessoku/problems/archive/a17.py
+++ python/books/tessoku/problems/archive/a17.py
@@ -1,20 +1,17 @@
 n = int(input())
-A = [0, 0] + list(map(int, input().split()))
-B = [0, 0, 0] + list(map(int, input().split()))
+A = [0]*2 + list(map(int, input().split()))
+B = [0]*3 + list(map(int, input().split()))
 DP = [0 for _ in range(n+1)]
 for i in range(1, n+1):
     if i == 1: DP[i] = 0
     elif i == 2: DP[i] = A[i]
     else: DP[i] = min(DP[i-1]+A[i], DP[i-2]+B[i])
-path = []
-point = n
+pos = n
+path = [n]
 while True:
-    path.append(point)
-    if point == 1:
-        break
-    elif DP[point] == DP[point-1] + A[point]:
-        point -= 1
-    else:
-        point -= 2
+    if DP[pos] - DP[pos-1] == A[pos]: pos -= 1
+    else: pos -= 2
+    path.append(pos)
+    if pos == 1: break
 print(len(path))
 print(*path[::-1], sep=" ")
