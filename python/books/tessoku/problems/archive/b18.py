2b7f4565db8216750f726389b6b167fba635a44e
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 17:18:00 +0900

    commit all

--- python/books/tessoku/problems/archive/b18.py
+++ python/books/tessoku/problems/archive/b18.py
@@ -1,26 +1,22 @@
-# from pprint import pprint
-
 n, s = map(int, input().split())
 A = [0] + list(map(int, input().split()))
 DP = [[False for _ in range(s+1)] for _ in range(n+1)]
 for i in range(n+1):
     for j in range(s+1):
-        if i == 0 and j == 0: DP[i][j] = True
+        if i == j == 0: DP[i][j] = True
         elif i == 0: DP[i][j] = False
-        elif DP[i-1][j]: DP[i][j] = True
-        elif j >= A[i]:
-            if DP[i-1][j-A[i]]:
-                DP[i][j] = True
+        else:
+            if DP[i-1][j]: DP[i][j] = True
+            elif A[i] <= j and DP[i-1][j-A[i]]: DP[i][j] = True
+            else: DP[i][j] = False
 if not DP[-1][-1]:
     print(-1)
     exit()
-
-path = []
-position = s
-for i in range(n, 0, -1):
-    if DP[i-1][position]:
-        continue
-    path.append(i)
-    position -= A[i]
-print(len(path))
-print(*path[::-1], sep=" ")
+cur = j
+choice = []
+for i in range(n, -1, -1):
+    if DP[i-1][j]: continue
+    choice.append(i)
+    cur -= A[i]
+print(len(choice))
+print(*choice[::-1], sep=" ")
