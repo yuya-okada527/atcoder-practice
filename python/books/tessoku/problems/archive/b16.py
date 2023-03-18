7c8c7ae46a22d9fbfb9edd414096ed4769fca6cc
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 16:47:35 +0900

    commit all

--- python/books/tessoku/problems/archive/b16.py
+++ python/books/tessoku/problems/archive/b16.py
@@ -1,9 +1,11 @@
 n = int(input())
 H = [0] + list(map(int, input().split()))
 DP = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    if i == 1: DP[i] = 0
-    elif i == 2: DP[i] = abs(H[i]-H[i-1])
-    else:
-        DP[i] = min(abs(H[i]-H[i-1])+DP[i-1], abs(H[i]-H[i-2])+DP[i-2])
+for j in range(1, n+1):
+    if j == 1: DP[j] = 0
+    elif j == 2: DP[j] = abs(H[j-1]-H[j])
+    else: DP[j] = min(
+        DP[j-1] + abs(H[j-1]-H[j]),
+        DP[j-2] + abs(H[j-2]-H[j])
+    )
 print(DP[-1])
