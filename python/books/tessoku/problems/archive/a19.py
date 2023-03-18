2badf1fe24a4b991fb642bd4c6429c292af396fa
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 22:22:26 +0900

    commit all

--- python/books/tessoku/problems/archive/a19.py
+++ python/books/tessoku/problems/archive/a19.py
@@ -1,5 +1,3 @@
-# from pprint import pprint
-
 n, w = map(int, input().split())
 W = [0]
 V = [0]
@@ -7,14 +5,12 @@ for _ in range(n):
     weight, value = map(int, input().split())
     W.append(weight)
     V.append(value)
-DP = [[-1 for _ in range(w+1)] for _ in range(n+1)]
+DP = [[-10**10 for _ in range(w+1)] for _ in range(n+1)]
 for i in range(n+1):
     for j in range(w+1):
-        if i == 0 and j == 0: DP[i][j] = 0
-        elif i == 0: DP[i][j] = -1
-        elif j >= W[i]:
-            DP[i][j] = max(DP[i-1][j], DP[i-1][j-W[i]] + V[i])
+        if i == j == 0: DP[i][j] = 0
+        elif i == 0: continue
         else:
-            DP[i][j] = DP[i-1][j]
-# pprint(DP)
+            if W[i] > j: DP[i][j] = DP[i-1][j]
+            else: DP[i][j] = max(DP[i-1][j], DP[i-1][j-W[i]]+V[i])
 print(max(DP[-1]))
