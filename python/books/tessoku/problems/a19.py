a108fc894a43122c944214f3a035bfb60d614f66
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 22:31:49 +0900

    commit all

---
+++ python/books/tessoku/problems/a19.py
@@ -0,0 +1,14 @@
+n, w = map(int, input().split())
+W = [0]
+V = [0]
+for _ in range(n):
+    weight, value = map(int, input().split())
+    W.append(weight)
+    V.append(value)
+DP = [[-10**12 for _ in range(w+1)] for _ in range(n+1)]
+DP[0][0] = 0
+for i in range(1, n+1):
+    for j in range(w+1):
+        if j-W[i] >= 0: DP[i][j] = max(DP[i-1][j], DP[i-1][j-W[i]]+V[i])
+        else: DP[i][j] = DP[i-1][j]
+print(max(DP[-1]))
