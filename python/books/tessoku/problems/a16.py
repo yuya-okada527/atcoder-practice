747ef5aee7284a57e0f6c6bd3df0795b0e4b0c83
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 16:39:17 +0900

    commit all

--- python/books/tessoku/problems/a16.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-A = [0] * 2 + list(map(int, input().split()))
-B = [0] * 3 + list(map(int, input().split()))
-DP = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    if i == 1: DP[i] = 0
-    elif i == 2: DP[i] = A[i]
-    else: DP[i] = min(DP[i-1]+A[i], DP[i-2]+B[i])
-print(DP[-1])
