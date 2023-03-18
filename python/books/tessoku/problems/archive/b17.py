69f12cbfcac6fa8156e54cd24cdd1d837eb7e32d
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-18 17:01:07 +0900

    commit all

--- python/books/tessoku/problems/archive/b17.py
+++ python/books/tessoku/problems/archive/b17.py
@@ -1,19 +1,18 @@
 n = int(input())
 H = [0] + list(map(int, input().split()))
 DP = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    if i == 1: DP[i] = 0
-    elif i == 2: DP[i] = abs(H[i-1]-H[i])
+for i in range(2, n+1):
+    if i == 2: DP[i] = abs(H[i-1]-H[i])
     else: DP[i] = min(
         DP[i-1] + abs(H[i-1]-H[i]),
         DP[i-2] + abs(H[i-2]-H[i])
     )
-path = []
-place = n
+pos = n
+path = [n]
 while True:
-    path.append(place)
-    if place == 1: break
-    elif DP[place] == DP[place-1] + abs(H[place-1]-H[place]): place -= 1
-    else: place -= 2
+    if DP[pos] - DP[pos-2] == abs(H[pos-2]-H[pos]): pos -= 2
+    else: pos -= 1
+    path.append(pos)
+    if pos == 1: break
 print(len(path))
 print(*path[::-1], sep=" ")
