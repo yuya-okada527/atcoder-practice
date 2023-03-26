e33fe0b1ade89dd32c6ed285b5f90081b4276a8d
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-26 17:44:08 +0900

    commit all

--- python/books/tessoku/problems/archive/b26.py
+++ python/books/tessoku/problems/archive/b26.py
@@ -4,10 +4,11 @@ primes = []
 for i in range(2, n+1):
     if deleted[i]: continue
     primes.append(i)
-    num = 2
+    coef = 2
     while True:
-        if i * num > n: break
-        else: deleted[i*num] = True
-        num += 1
+        num = i * coef
+        if num > n: break
+        deleted[num] = True
+        coef += 1
 for p in primes:
     print(p)
