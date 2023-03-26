df5429664d71792da99d401fe0b1f50c67e69d07
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-26 16:54:17 +0900

    commit all

--- python/books/tessoku/problems/a18.py
+++
@@ -1,9 +0,0 @@
-n, s = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-DP = [[False for _ in range(s+1)] for _ in range(n+1)]
-DP[0][0] = True
-for i in range(1, n+1):
-    for j in range(s+1):
-        DP[i][j] = DP[i-1][j] or (j >= A[i] and DP[i-1][j-A[i]])
-if DP[-1][-1]: print("Yes")
-else: print("No")
--- python/books/tessoku/problems/archive/a18.py
+++ python/books/tessoku/problems/archive/a18.py
@@ -1,13 +1,9 @@
 n, s = map(int, input().split())
 A = [0] + list(map(int, input().split()))
 DP = [[False for _ in range(s+1)] for _ in range(n+1)]
-for i in range(n+1):
+DP[0][0] = True
+for i in range(1, n+1):
     for j in range(s+1):
-        if i == j == 0: DP[i][j] = True
-        elif i == 0: DP[i][j] = False
-        else:
-            if DP[i-1][j]: DP[i][j] = True
-            elif A[i] <= j and DP[i-1][j-A[i]]: DP[i][j] = True
-            else: DP[i][j] = False
+        DP[i][j] = DP[i-1][j] or (j >= A[i] and DP[i-1][j-A[i]])
 if DP[-1][-1]: print("Yes")
 else: print("No")
