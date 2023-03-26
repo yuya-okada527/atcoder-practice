97637373abddb4179116587bb5cfd0a14eda94a0
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-26 17:37:26 +0900

    commit all

--- python/books/tessoku/problems/archive/a20.py
+++ python/books/tessoku/problems/archive/a20.py
@@ -1,18 +1,11 @@
-from pprint import pprint
 s = "x" + input()
 t = "x" + input()
 DP = [[0 for _ in range(len(t))] for _ in range(len(s))]
-for i in range(len(s)):
+for i in range(1, len(s)):
     for j in range(len(t)):
-        if i == 0: continue
-        if j == 0: DP[i][j] = DP[i-1][j]
+        if j == 0:
+            DP[i][j] = DP[i-1][j]
         else:
-            if s[i] == t[j]:
-                DP[i][j] = max(
-                    DP[i-1][j],    # 上から
-                    DP[i][j-1],    # 左から
-                    DP[i-1][j-1]+1 # ななめ
-                )
-            else:
-                DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+            if s[i] == t[j]: DP[i][j] = max(DP[i-1][j], DP[i][j-1], DP[i-1][j-1]+1)
+            else: DP[i][j] = max(DP[i-1][j], DP[i][j-1])
 print(DP[-1][-1])
