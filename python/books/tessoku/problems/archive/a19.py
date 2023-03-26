07336a57bc812e7559d3dca9136663de44dcf9dd
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-26 17:12:10 +0900

    commit all

--- python/books/tessoku/problems/archive/a19.py
+++ python/books/tessoku/problems/archive/a19.py
@@ -5,12 +5,10 @@ for _ in range(n):
     weight, value = map(int, input().split())
     W.append(weight)
     V.append(value)
-DP = [[-10**10 for _ in range(w+1)] for _ in range(n+1)]
-for i in range(n+1):
+DP = [[-10**12 for _ in range(w+1)] for _ in range(n+1)]
+DP[0][0] = 0
+for i in range(1, n+1):
     for j in range(w+1):
-        if i == j == 0: DP[i][j] = 0
-        elif i == 0: continue
-        else:
-            if W[i] > j: DP[i][j] = DP[i-1][j]
-            else: DP[i][j] = max(DP[i-1][j], DP[i-1][j-W[i]]+V[i])
+        if j-W[i] >= 0: DP[i][j] = max(DP[i-1][j], DP[i-1][j-W[i]]+V[i])
+        else: DP[i][j] = DP[i-1][j]
 print(max(DP[-1]))
