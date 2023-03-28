61affe9802419685a768bd9cce67c5d880bb2981
yuya-okada527 <yuya.okada527@gmail.com>
2023-03-28 22:51:04 +0900

    commit all

--- python/books/tessoku/problems/a37.py
+++ python/books/tessoku/problems/a37.py
@@ -2,9 +2,7 @@ n, m, b = map(int, input().split())
 A = list(map(int, input().split()))
 C = list(map(int, input().split()))
 ans = 0
-for a in A:
-    ans += a * m
-for c in C:
-    ans += c * n
+for a in A: ans += a * m
+for c in C: ans += c * n
 ans += n * m * b
 print(ans)
