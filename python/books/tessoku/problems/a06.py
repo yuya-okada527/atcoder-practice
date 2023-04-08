6a0935c73455bde8182cc49a34c2b0e0d9995b98
yuya-okada527 <yuya.okada527@gmail.com>
2023-04-08 21:10:51 +0900

    commit all

--- python/books/tessoku/problems/a06.py
+++
@@ -1,8 +0,0 @@
-n, q = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-Z = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    Z[i] = Z[i-1] + A[i]
-LR = [map(int, input().split()) for _ in range(q)]
-for l, r in LR:
-    print(Z[r] - Z[l-1])
--- python/books/tessoku/problems/archive/a06.py
+++ python/books/tessoku/problems/archive/a06.py
@@ -3,9 +3,6 @@ A = [0] + list(map(int, input().split()))
 Z = [0 for _ in range(n+1)]
 for i in range(1, n+1):
     Z[i] = Z[i-1] + A[i]
-LR = []
-for _ in range(q):
-    l, r = map(int, input().split())
-    LR.append((l, r))
+LR = [map(int, input().split()) for _ in range(q)]
 for l, r in LR:
     print(Z[r] - Z[l-1])
