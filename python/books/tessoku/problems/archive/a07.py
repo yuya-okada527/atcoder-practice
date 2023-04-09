9f97fb740c1d6c510660e926cbb28f3405675b0c
yuya-okada527 <yuya.okada527@gmail.com>
2023-04-09 13:58:55 +0900

    commit all

--- python/books/tessoku/problems/archive/a07.py
+++ python/books/tessoku/problems/archive/a07.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 d = int(input())
 n = int(input())
 DIFF = [0 for _ in range(d+2)]
@@ -6,7 +8,8 @@ for _ in range(n):
     DIFF[l]   += 1
     DIFF[r+1] -= 1
 Z = [0 for _ in range(d+2)]
-for i in range(1, d+1):
+for i in range(1, d+2):
     Z[i] = Z[i-1] + DIFF[i]
+
 for z in Z[1:-1]:
     print(z)
