d1eb35b9cdede85565a3a3d8728fbc0781be5564
yuya-okada527 <yuya.okada527@gmail.com>
2023-05-14 13:22:25 +0900

    Add function to judge if two matrices are the same

--- python/contest/abc/1/300/b.py
+++ python/contest/abc/1/300/b.py
@@ -1,2 +1,55 @@
+from pprint import pprint as pp
+from copy import deepcopy
+
+def is_same(a, b):
+    for i in range(h):
+        for j in range(w):
+            if a[i][j] != b[i][j]:
+                return False
+    return True
+
+
+def shift_verticaly(a):
+    b = [["" for _ in range(w)] for _ in range(h)]
+    for i in range(h):
+        for j in range(w):
+            b[i][(j+1)%w] = a[i][j]
+    return b
+
+
+def shift_horizontaly(a):
+    b = [["" for _ in range(w)] for _ in range(h)]
+    for i in range(h):
+        for j in range(w):
+            b[(i+1)%h][j] = a[i][j]
+    return b
+
+
+def judge(a, b):
+    for i in range(h):
+        # print(f"{i=}")
+        copy_a = deepcopy(a)
+        while True:
+            if i == 0: break
+            copy_a = shift_verticaly(copy_a)
+            i -= 1
+        for j in range(w):
+            # print(f"{j=}")
+            copy_a2 = deepcopy(copy_a)
+            while True:
+                if j == 0: break
+                copy_a2 = shift_horizontaly(copy_a2)
+                j -= 1
+            # pp(copy_a2)
+            if is_same(copy_a2, b):
+                return True
+    return False
+
+
 h, w = map(int, input().split())
-A = [[]]
+A = [[i for i in input()] for _ in range(h)]
+B = [[i for i in input()] for _ in range(h)]
+if judge(A, B):
+    print("Yes")
+else:
+    print("No")
