f05d0dc1dbddeb5668dcbb0e75c85fd45ed305a4
yuya-okada527 <yuya.okada527@gmail.com>
2023-06-29 09:52:20 +0900

    Refactor code to improve readability and efficiency

--- python/books/tessoku/problems/archive/a06.py
+++ python/books/tessoku/problems/archive/a06.py
@@ -3,6 +3,10 @@ A = [0] + list(map(int, input().split()))
 Z = [0 for _ in range(n+1)]
 for i in range(1, n+1):
     Z[i] = Z[i-1] + A[i]
-LR = [map(int, input().split()) for _ in range(q)]
+LR = []
+for _ in range(q):
+    l, r = map(int, input().split())
+    LR.append((l, r))
 for l, r in LR:
-    print(Z[r] - Z[l-1])
+    ans = Z[r] - Z[l-1]
+    print(ans)
