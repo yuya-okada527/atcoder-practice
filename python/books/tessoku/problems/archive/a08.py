ab8fec608857eb68aa937b65e277869806623997
yuya-okada527 <yuya.okada527@gmail.com>
2023-07-03 09:09:48 +0900

    Refactor a08.py for better readability

--- python/books/tessoku/problems/archive/a08.py
+++ python/books/tessoku/problems/archive/a08.py
@@ -1,22 +1,17 @@
-# from pprint import pprint as print
-
 h, w = map(int, input().split())
-X = [[0 for _ in range(w+1)] for _ in range(h+1)]
+HW = [[0 for _ in range(w+1)] for _ in range(h+1)]
 for i in range(1, h+1):
-    for j, x in enumerate(input().split(), start=1):
-        X[i][j] = int(x)
+    for j, v in enumerate(map(int, input().split()), start=1):
+        HW[i][j] = v
 Z = [[0 for _ in range(w+1)] for _ in range(h+1)]
 for i in range(1, h+1):
     for j in range(1, w+1):
-        Z[i][j] = Z[i-1][j] + X[i][j]
+        Z[i][j] = Z[i-1][j] + HW[i][j]
 for i in range(1, h+1):
-    for j in range(1, w+1):
+    for j in range(w+1):
         Z[i][j] = Z[i][j-1] + Z[i][j]
 q = int(input())
-Q = []
 for _ in range(q):
     a, b, c, d = map(int, input().split())
-    ans = Z[c][d] - Z[a-1][d] - Z[c][b-1] + Z[a-1][b-1]
+    ans = Z[c][d] - Z[c][b-1] - Z[a-1][d] + Z[a-1][b-1]
     print(ans)
-# print(X)
-# print(Z)
