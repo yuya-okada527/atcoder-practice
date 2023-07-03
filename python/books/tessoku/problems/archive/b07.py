8499876412a50133110f7fac804de3405fee17ea
yuya-okada527 <yuya.okada527@gmail.com>
2023-07-03 09:01:19 +0900

    Refactor b07.py to improve readability and remove unnecessary code

--- python/books/tessoku/problems/archive/b07.py
+++ python/books/tessoku/problems/archive/b07.py
@@ -5,8 +5,9 @@ for _ in range(n):
     l, r = map(int, input().split())
     DIFF[l] += 1
     DIFF[r] -= 1
-Z = [0 for _ in range(t+2)]
-for i in range(t+2):
-    Z[i] = Z[i-1] + DIFF[i]
-for i in Z[:-2]:
-    print(i)
+Z = [0 for _ in range(t+1)]
+for i in range(t+1):
+    if i == 0: Z[i] = DIFF[i]
+    else: Z[i] = Z[i-1] + DIFF[i]
+for z in Z[:-1]:
+    print(z)
