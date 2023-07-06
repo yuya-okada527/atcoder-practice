48082cff9166c62b352ddc8fdf420cbf3bd87cff
yuya-okada527 <yuya.okada527@gmail.com>
2023-07-07 08:28:16 +0900

    Refactor binary search function and remove unnecessary code."Refactor binary search function and remove unnecessary code."

--- python/books/tessoku/problems/archive/b11.py
+++ python/books/tessoku/problems/archive/b11.py
@@ -1,8 +1,16 @@
-from bisect import bisect_left
+def f(x, lst):
+    left = 0
+    right = len(lst)
+    while left < right:
+        mid = (left+right)//2
+        if lst[mid] == x: return mid
+        elif lst[mid] > x: right = mid
+        else: left = mid + 1
+    return left
 
 n = int(input())
 A = sorted(list(map(int, input().split())))
 q = int(input())
 for _ in range(q):
     x = int(input())
-    print(bisect_left(A, x))
+    print(f(x, A))
