ac3969e73a54bc71db78db131ad292e4d42f8369
yuya-okada527 <yuya.okada527@gmail.com>
2023-07-19 09:45:54 +0900

    Refactor code for a16.py to improve readability and remove unnecessary lines

--- python/books/tessoku/problems/a16.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-A = [0] * 2 + list(map(int, input().split()))
-B = [0] * 3 + list(map(int, input().split()))
-DP = [0 for _ in range(n+1)]
-for i in range(2, n+1):
-    if i == 2: DP[i] = A[i]
-    else: DP[i] = min(DP[i-1]+A[i], DP[i-2]+B[i])
-print(DP[-1])
--- python/books/tessoku/problems/archive/a16.py
+++ python/books/tessoku/problems/archive/a16.py
@@ -2,8 +2,7 @@ n = int(input())
 A = [0] * 2 + list(map(int, input().split()))
 B = [0] * 3 + list(map(int, input().split()))
 DP = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    if i == 1: DP[i] = 0
-    elif i == 2: DP[i] = A[i]
+for i in range(2, n+1):
+    if i == 2: DP[i] = A[i]
     else: DP[i] = min(DP[i-1]+A[i], DP[i-2]+B[i])
 print(DP[-1])
