804ad7e34304f8983ad0e5c209526e86fb6196d5
yuya-okada527 <yuya.okada527@gmail.com>
2023-11-19 22:44:15 +0900

    add test implementations

--- python/contest/abc/1/329/b.py
+++ python/contest/abc/1/329/b.py
@@ -4,7 +4,7 @@ import unittest
 
 def f(lst: List[int]):
     assert isinstance(lst, list)
-    assert len(lst) != 0
+    assert len(set(lst)) >= 2
     return sorted(set(lst))[-2]
 
 
@@ -16,7 +16,7 @@ def main():
 
 
 class UnitTest(unittest.TestCase):
-    def test_リスト以外(self):
+    def test_リスト以外の場合(self):
         not_list_args = [None, "string", 1, True, {}]
         for args in not_list_args:
             with self.assertRaises(AssertionError):
@@ -26,11 +26,28 @@ class UnitTest(unittest.TestCase):
         with self.assertRaises(AssertionError):
             f([])
 
-    def test_ソート済み(self):
+    def test_要素数1の配列の場合(self):
+        with self.assertRaises(AssertionError):
+            f([1])
+
+    def test_要素数2の配列の場合(self):
+        self.assertEqual(f([1, 2]), 1)
+
+    def test_ソート済みの場合(self):
         self.assertEqual(f([1, 2, 3]), 2)
 
-    def test_has_equal_num(self):
+    def test_ソートされていない場合(self):
+        self.assertEqual(f([6, 3, 1, 2]), 3)
+
+    def test_重複あり_最高値(self):
         self.assertEqual(f([1, 2, 2]), 1)
+
+    def test_重複あり_2番目(self):
+        self.assertEqual(f([1, 1, 2]), 1)
+
+    def test_全て同じ値(self):
+        with self.assertRaises(AssertionError):
+            f([1, 1, 1])
 
 
 if __name__ == "__main__":
