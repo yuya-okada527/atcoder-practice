89e9c792908dbb7464c88f81a413323c93481be2
yuya-okada527 <yuya.okada527@gmail.com>
2023-11-19 22:50:15 +0900

    add comment annotation

--- python/contest/abc/1/329/b.py
+++ python/contest/abc/1/329/b.py
@@ -16,35 +16,44 @@ def main():
 
 
 class UnitTest(unittest.TestCase):
+    # 引数の型からの考察
     def test_リスト以外の場合(self):
         not_list_args = [None, "string", 1, True, {}]
         for args in not_list_args:
             with self.assertRaises(AssertionError):
                 f(args)
 
+    # 引数の型からの考察
     def test_空配列の場合(self):
         with self.assertRaises(AssertionError):
             f([])
 
+    # 実装からの考察
     def test_要素数1の配列の場合(self):
         with self.assertRaises(AssertionError):
             f([1])
 
+    # 実装からの考察
     def test_要素数2の配列の場合(self):
         self.assertEqual(f([1, 2]), 1)
 
+    # 実装からの考察
     def test_ソート済みの場合(self):
         self.assertEqual(f([1, 2, 3]), 2)
 
+    # 実装からの考察
     def test_ソートされていない場合(self):
         self.assertEqual(f([6, 3, 1, 2]), 3)
 
+    # 仕様からの考察
     def test_重複あり_最高値(self):
         self.assertEqual(f([1, 2, 2]), 1)
 
+    # 仕様からの考察の派生
     def test_重複あり_2番目(self):
         self.assertEqual(f([1, 1, 2]), 1)
 
+    # 仕様からの考察の派生
     def test_全て同じ値(self):
         with self.assertRaises(AssertionError):
             f([1, 1, 1])
