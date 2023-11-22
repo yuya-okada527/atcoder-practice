fed52c2f8e54ed3f7935ee6ba47ee5451d4d5dd4
yuya-okada527 <yuya.okada527@gmail.com>
2023-11-22 21:42:02 +0900

    add a

---
+++ java/contest/abc/329/A.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class A {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    List<String> list = new ArrayList<String>();
+  }
+}
